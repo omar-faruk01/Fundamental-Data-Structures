2f35fbe6dd121c2eb206b70e460363e8feafb182
Omar Faruk <70847377+omar-faruk01@users.noreply.github.com>
2021-02-24 18:52:52 -0500

    Linked List
    Creating a linked list with three nodes

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -1,1 +1,58 @@
+//==========================================================
+//Linked List
 
+
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout"
+
+class Node
+{
+public:
+	int data;
+	Node* next;
+};
+
+void insert(int data, int n) 
+{
+	
+}
+void printList(Node* n)
+{
+	while (n != NULL)
+	{
+		cout << n->data << " ";
+		n = n->next;
+	}
+}
+
+int main()
+{
+	// Declare nodes
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+	// Allocate 3 nodes
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	// Assign Head node
+	head->data = 5;
+	head->next = second;
+
+	// Assign second node
+	second->data = 10;
+	second->next = third;
+
+	// Assign third node
+	third->data = 15;
+	third->next = NULL;
+
+	// Print linked list
+	printList(head);
+
+}
